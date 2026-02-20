025e962de7049700a451b5434f064ebb8514c05b
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-02-20 16:17:29 +0530

    [Abuzar] : Added initial setup for Quantity Measurement App Feature One Feet Equality

---
+++ src/main/java/com/app/quantitymeasurement/Feet.java
@@ -0,0 +1,5 @@
+package com.app.quantitymeasurement;
+
+public class Feet {
+
+}
---
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,13 @@
+package com.app.quantitymeasurement;
+
+
+public class QuantityMeasurementApp {
+	
+	
+	
+	
+	
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
