54a29433c1f6236041d949ab216a39f69b38f6fa
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-02-20 16:22:59 +0530

    [Abuzar] : Added Feature One of Quantity Measurement App with Feet Class

--- src/main/java/com/app/quantitymeasurement/Feet.java
+++ src/main/java/com/app/quantitymeasurement/Feet.java
@@ -1,5 +1,43 @@
 package com.app.quantitymeasurement;
 
-public class Feet {
+import java.util.Objects;
 
+
+public  class Feet {
+
+    private final double value; // Immutable field
+
+    public Feet(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        // Step 1: Same reference check (Reflexive)
+        if (this == obj) {
+            return true;
+        }
+
+        // Step 2: Null and type check
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        // Step 3: Safe casting
+        Feet other = (Feet) obj;
+
+        // Step 4: Proper double comparison
+        return Double.compare(this.value, other.value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
+
--- src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,13 +1,16 @@
 package com.app.quantitymeasurement;
 
-
 public class QuantityMeasurementApp {
-	
-	
-	
-	
-	
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	
+    	// Feature One : Feet
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        boolean result = f1.equals(f2);
+
+        System.out.println("Are they equal? " + result);
     }
 }
