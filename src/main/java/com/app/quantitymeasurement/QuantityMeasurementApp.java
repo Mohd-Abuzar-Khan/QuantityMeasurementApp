afb2d33fe74967caa407d42723bc2ff6097b116d
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-02-21 12:09:40 +0530

    [Abuzar] : Updating the structure for UC3 and Feature Third Generic Length

--- src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,13 +1,29 @@
 package com.app.quantitymeasurement;
 
-
 public class QuantityMeasurementApp {
-	
-	
-	
-	
-	
+
+    public static boolean compareFeet(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
+
+    public static boolean compareInches(double v1, double v2) {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+        boolean feetResult = compareFeet(1.0, 1.0);
+        boolean inchResult = compareInches(1.0, 1.0);
+
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + feetResult + ")");
+
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal (" + inchResult + ")");
     }
 }
