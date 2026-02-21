4d55f60d6a304bab2aba1a865a983e8a6bb0a0c0
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-02-21 11:11:13 +0530

    [Abuzar] : Added Feature Two Inches Equality in Quantity Measurement App

--- src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,15 +2,28 @@ package com.app.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
+    public static boolean compareFeet(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
+
+    public static boolean compareInches(double v1, double v2) {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
+    }
 
     public static void main(String[] args) {
-    	
-    	// Feature One : Feet
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
 
-        boolean result = f1.equals(f2);
 
-        System.out.println("Are they equal? " + result);
+        boolean feetResult = compareFeet(1.0, 1.0);
+        boolean inchResult = compareInches(1.0, 1.0);
+
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + feetResult + ")");
+
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal (" + inchResult + ")");
     }
 }
