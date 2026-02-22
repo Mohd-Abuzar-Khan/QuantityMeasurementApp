afe9d941e8bf88428451fd51de258f0b836a4581
Mohd-Abuzar-Khan <abbuzar982@gmail.com>
2026-02-22 17:41:13 +0530

    [Abuzar] : Updating the structure for day Feature Inche Equality

--- src/test/java/com/app/quantitymeasurement/QuantityMeasurementAppUC2Test.java
+++ src/test/java/com/app/quantitymeasurement/QuantityMeasurementAppUC2Test.java
@@ -3,76 +3,59 @@ package com.app.quantitymeasurement;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class QuantityMeasurementAppUC2Test {
+class QuantityMeasurementAppTest {
 
-    // =========================
-    // FEET TESTS
-    // =========================
-
+	// Feet Testing for UC1
     @Test
-    void testFeetEquality_SameValue() {
+    void feet_SameValue() {
         assertTrue(QuantityMeasurementApp.compareFeet(1.0, 1.0));
     }
 
     @Test
-    void testFeetEquality_DifferentValue() {
+    void feet_DifferentValue() {
         assertFalse(QuantityMeasurementApp.compareFeet(1.0, 2.0));
     }
 
     @Test
-    void testFeetEquality_NullComparison() {
+    void feet_NullComparison() {
         Feet f = new Feet(1.0);
         assertFalse(f.equals(null));
     }
 
     @Test
-    void testFeetEquality_SameReference() {
+    void feet_SameReference() {
         Feet f = new Feet(1.0);
         assertTrue(f.equals(f));
     }
 
-    @Test
-    void testFeetEquality_InvalidInput() {
-        assertThrows(IllegalArgumentException.class,
-                () -> new Feet(Double.NaN));
-    }
 
-    // =========================
-    // INCHES TESTS
-    // =========================
-
+    
+    // Inches Testing for UC2
     @Test
-    void testInchesEquality_SameValue() {
+    void Inches_SameValue() {
         assertTrue(QuantityMeasurementApp.compareInches(5.0, 5.0));
     }
 
     @Test
-    void testInchesEquality_DifferentValue() {
+    void Inches_DifferentValue() {
         assertFalse(QuantityMeasurementApp.compareInches(5.0, 10.0));
     }
 
     @Test
-    void testInchesEquality_NullComparison() {
+    void Inches_NullComparison() {
         Inches i = new Inches(2.0);
         assertFalse(i.equals(null));
     }
 
     @Test
-    void testInchesEquality_SameReference() {
+    void Inches_SameReference() {
         Inches i = new Inches(2.0);
         assertTrue(i.equals(i));
     }
 
-    @Test
-    void testInchesEquality_InvalidInput() {
-        assertThrows(IllegalArgumentException.class,
-                () -> new Inches(Double.POSITIVE_INFINITY));
-    }
 
-    // =========================
-    // TYPE SAFETY TEST
-    // =========================
 
+    // Type Safety Testing
     @Test
     void testFeetNotEqualToInches() {
         Feet f = new Feet(1.0);
